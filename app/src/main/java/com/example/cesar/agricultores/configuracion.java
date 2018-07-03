51b604620f8369fc4aac22f847d574e5cc7e73b5
satcosta <informatica2@hcostadealmeria.es>
2018-07-03 12:55:16 +0200

    primera actu

--- app/src/main/java/com/example/cesar/agricultores/configuracion.java
+++ app/src/main/java/com/example/cesar/agricultores/configuracion.java
@@ -11,15 +11,22 @@ import android.view.ViewGroup;
  * Created by Cesar on 09/05/2018.
  */
 
-public class configuracion extends Fragment {
+public class configuracion extends Fragment implements AsyncResponse{
     Context context;
+    private webphp php;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View vi=inflater.inflate(R.layout.principal, container, false);
+        php=new webphp();
+        php.delegate = configuracion.this;
+        //php.execute("http://hcostadealmeria.com/tecnicos/receta2.php?id=" + ids.get(tecnicos.getSelectedItemPosition()) + "&fecha=" + nfecha2);
+
         return vi;
+    }
+    public void processFinish(String output) {
     }
 
 }
