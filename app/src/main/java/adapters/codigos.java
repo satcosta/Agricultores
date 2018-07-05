0109fa3a8cb9e07968dc4109a1033a11e04d02f1
satcosta <informatica2@hcostadealmeria.es>
2018-07-05 13:09:04 +0200

    primera actu

--- app/src/main/java/adapters/codigos.java
+++ app/src/main/java/adapters/codigos.java
@@ -3,12 +3,15 @@ package adapters;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.cesar.agricultores.R;
@@ -42,19 +45,37 @@ public class codigos extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View vi=convertView;
         if(convertView==null){
-            LayoutInflater mInflater = (LayoutInflater) context
-                    .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             vi = mInflater.inflate(R.layout.cuentas_adapter, null);
         }
         // vi = inflater.inflate(R.layout.noticias_datos, null);
+        final View vi2=vi;
         TextView codigo = (TextView) vi.findViewById(R.id.codigo); // title
         TextView nombre = (TextView) vi.findViewById(R.id.nombre);
-
+        ImageButton btn_del= (ImageButton) vi.findViewById(R.id.btn_del);
         HashMap<String, String> song = new HashMap<String, String>();
         song = data.get(position);
-
         codigo.setText(song.get(configuracion.KEY_CODIGO));
         nombre.setText(song.get(configuracion.KEY_NOMBRE));
+        btn_del.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new AlertDialog.Builder(context)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setTitle(R.string.c_del1)
+                        .setMessage(R.string.c_del2)
+                        .setPositiveButton("Si", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                vi2.setVisibility(View.GONE);
+                            }
+
+                        })
+                        .setNegativeButton("No", null)
+                        .show();
+
+            }
+        });
         /*
         Nomenvase.setTextColor(Color.WHITE);
         cantidad.setTextColor(Color.WHITE);
@@ -62,6 +83,7 @@ public class codigos extends BaseAdapter {
         vi.setBackgroundColor(Color.parseColor("#315b8f"));
         cantidad.setTypeface(null, Typeface.BOLD);
         cantidad.setTextSize(16);*/
+        vi=vi2;
         return vi;
     }
 }
--- app/src/main/java/com/example/cesar/agricultores/configuracion.java
+++ app/src/main/java/com/example/cesar/agricultores/configuracion.java
@@ -2,11 +2,18 @@ package com.example.cesar.agricultores;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import adapters.codigos;
 
 /**
  * Created by Cesar on 09/05/2018.
@@ -17,20 +24,41 @@ public class configuracion extends Fragment implements AsyncResponse{
     private webphp php;
     public static final String KEY_CODIGO = "codigo";
     public static final String KEY_NOMBRE = "nombre";
+    FloatingActionButton btn_add;
+    private ListView list;
+    ArrayList<HashMap<String, String>> songsList = new ArrayList<HashMap<String, String>>();
+    codigos listAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         final View vi=inflater.inflate(R.layout.cuentas, container, false);
-        php=new webphp();
-        php.delegate = configuracion.this;
-        php.execute("http://192.168.0.163/login.php","1","1234","123456789");
+        list= (ListView) vi.findViewById(R.id.listc);
+        btn_add = (FloatingActionButton) vi.findViewById(R.id.btn_add);
+        songsList.clear();
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("TAG", "Hacer algo");
+                php=new webphp();
+                php.delegate = configuracion.this;
+                php.execute("http://192.168.0.163/login.php","1","1234","123456789");
+            }
+        });
+
 
         return vi;
+
     }
     public void processFinish(String output) {
         Log.i("RESULTADO", output);
+        HashMap<String, String> map = new HashMap<String, String>();
+        // adding each child node to HashMap key => value
+        map.put(KEY_CODIGO, "1");
+        map.put(KEY_NOMBRE, output);
+        songsList.add(map);
+        listAdapter=new codigos(getContext(), songsList);
+        list.setAdapter(listAdapter);
     }
 
 }
